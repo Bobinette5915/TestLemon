13f3979e1a88395a1f1892016349607a4aa903da
Flav Bobinette <flavien.grammont@gmail.com>
2025-08-29 12:44:51 +0200

    premier commit config

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'api_key' => 'Token api', //a remplacer par le token delivré par l'api
+    'base_url' => 'https://api.themoviedb.org/3',
+    'log_file' => __DIR__ . '/recherche.log'
+];
+?>
